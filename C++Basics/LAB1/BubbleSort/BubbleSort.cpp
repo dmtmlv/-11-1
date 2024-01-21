624acbb85a8adbffef1d01710e5a344145b0905c
dmtmlv <154061230+dmtmlv@users.noreply.github.com>
2024-01-21 13:39:37 +0300

    Add files via upload

---
+++ C++Basics/LAB1/BubbleSort/BubbleSort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j]; //меняем элементы местами
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "Исходный массив: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    bubbleSort(arr, n);
+
+    std::cout << "\nОтсортированный массив: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    return 0;
+}
