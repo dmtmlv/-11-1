03d03c02164e15d5534a41462403878e18d3f43c
dmtmlv <154061230+dmtmlv@users.noreply.github.com>
2024-01-26 00:19:26 +0300

    Add files via upload

---
+++ OOP/LAB1/Time/Time.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+class Time {
+private:
+    int hours;
+    int minutes;
+    int seconds;
+
+public:
+    // Конструктор с параметрами
+    Time(int h, int m, int s);
+
+    // Геттеры и сеттеры
+    int getHours() const;
+    int getMinutes() const;
+    int getSeconds() const;
+    void setHours(int h);
+    void setMinutes(int m);
+    void setSeconds(int s);
+
+    // Сложение времен
+    Time operator+(const Time& t) const;
+
+    // Вычитание времен
+    Time operator-(const Time& t) const;
+
+    // Добавление времени в минутах
+    void addMinutes(int m);
+
+    // Перевод времени в секунды
+    int toSeconds() const;
+
+    // Преобразование в строку (в формате HH:MM:SS)
+    std::string toString() const;
+};
+
