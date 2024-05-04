04e750c781cdbc669501a522266ec584143bdf7d
dmtmlv <154061230+dmtmlv@users.noreply.github.com>
2024-05-04 06:14:28 +0300

    Update and rename main.cpp to editor.cpp

---
+++ OOP/LAB2/editor.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+// Command Interface
+class Command {
+public:
+    virtual void execute() = 0;
+    virtual void undo() = 0;
+};
+
+// Concrete Command for typing a character
+class TypeCommand : public Command {
+private:
+    std::string& buffer;
+    char typedChar;
+    int cursorPos;
+
+public:
+    TypeCommand(std::string& buf, char c, int pos) : buffer(buf), typedChar(c), cursorPos(pos) {}
+
+    void execute() override {
+        buffer.insert(cursorPos, 1, typedChar);
+        cursorPos++;
+    }
+
+    void undo() override {
+        buffer.erase(cursorPos - 1, 1);
+        cursorPos--;
+    }
+};
+
+// Concrete Command for shifting cursor left
+class ShiftLeftCommand : public Command {
+private:
+    int& cursorPos;
+
+public:
+    ShiftLeftCommand(int& pos) : cursorPos(pos) {}
+
+    void execute() override {
+        cursorPos = std::max(0, cursorPos - 1);
+    }
+
+    void undo() override {
+        cursorPos++;
+    }
+};
+
+// Concrete Command for shifting cursor right
+class ShiftRightCommand : public Command {
+private:
+    int& cursorPos;
+    int bufferSize;
+
+public:
+    ShiftRightCommand(int& pos, int bufSize) : cursorPos(pos), bufferSize(bufSize) {}
+
+    void execute() override {
+        cursorPos = std::min(bufferSize, cursorPos + 1);
+    }
+
+    void undo() override {
+        cursorPos--;
+    }
+};
+
+// TextEditor Class
+class TextEditor {
+private:
+    std::string buffer;
+    int cursorPos;
+    std::stack<Command*> history;
+    std::stack<Command*> redoHistory;
+
+public:
+    TextEditor() : cursorPos(0) {}
+
+    void Type(char c) {
+        Command* command = new TypeCommand(buffer, c, cursorPos);
+        command->execute();
+        history.push(command);
+    }
+
+    void ShiftLeft() {
+        Command* command = new ShiftLeftCommand(cursorPos);
+        command->execute();
+        history.push(command);
+    }
+
+    void ShiftRight() {
+        Command* command = new ShiftRightCommand(cursorPos, buffer.size());
+        command->execute();
+        history.push(command);
+    }
+
+    void Backspace() {
+        if (cursorPos > 0) {
+            buffer.erase(cursorPos - 1, 1);
+            cursorPos--;
+        }
+    }
+
+    void Undo() {
+        if (!history.empty()) {
+            Command* command = history.top();
+            history.pop();
+            command->undo();
+            redoHistory.push(command);
+        }
+    }
+
+    void Redo() {
+        if (!redoHistory.empty()) {
+            Command* command = redoHistory.top();
+            redoHistory.pop();
+            command->execute();
+            history.push(command);
+        }
+    }
+
+    const std::string& GetText() const {
+        return buffer;
+    }
+};
+
+int main() {
+    TextEditor editor;
+
+    editor.Type('o');
+    editor.Type('l');
+    editor.Type('l');
+    editor.Type('e');
+    editor.Type('H');
+    editor.ShiftLeft();
+    editor.Backspace();
+    editor.Undo();
+    editor.Redo();
+
+    std::cout << editor.GetText() << std::endl;
+
+    return 0;
+}
