ce6a18a771ea0f270f177b6e7ec0b11959df7d9b
dmtmlv <154061230+dmtmlv@users.noreply.github.com>
2024-05-13 13:14:59 +0300

    Update any.cpp

--- OOP/LAB3/any.cpp
+++ OOP/LAB3/any.cpp
@@ -11,7 +11,7 @@ int main()
     any c(std::string("Hello world!"));
     any d(0.5);
 
-    if (a.is_initialized()) { // проверка работоспособности функции проверки инициализации
+    if (a.is_initialized()) { // проверка инициализации
         std::cout << "Объект 'a' инициализирован" << std::endl;
     }
     else {
@@ -21,8 +21,8 @@ int main()
     try {
         std::cout << b.cast<int>() << std::endl;
         std::cout << d.cast<double>() << std::endl;
-        std::cout << c.cast<double>() << std::endl; // вызывет исключение bad_cast
-        std::cout << a.cast<int>() << std::endl;  // вызовет исключение any is not initialized
+        std::cout << c.cast<double>() << std::endl; // вызов исключения bad_cast
+        std::cout << a.cast<int>() << std::endl;  // вызов исключения any is not initialized
     }
     catch (const std::exception& e) {
         std::cout << "Исключение: " << e.what() << std::endl;
