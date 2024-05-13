da3ce525f4d58f703b900048919884f733b8f61a
dmtmlv <154061230+dmtmlv@users.noreply.github.com>
2024-05-13 11:20:46 +0300

    Update and rename std::any.cpp to any.cpp

---
+++ OOP/LAB3/any.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <typeinfo>
+#include"any.h"
+#include<iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    any a;
+    any b(13);
+    any c(std::string("Hello world!"));
+    any d(0.5);
+
+    if (a.is_initialized()) { // проверка работоспособности функции проверки инициализации
+        std::cout << "Объект 'a' инициализирован" << std::endl;
+    }
+    else {
+        std::cout << "Объект 'a' не инициализирован" << std::endl;
+    }
+
+    try {
+        std::cout << b.cast<int>() << std::endl;
+        std::cout << d.cast<double>() << std::endl;
+        std::cout << c.cast<double>() << std::endl; // вызывет исключение bad_cast
+        std::cout << a.cast<int>() << std::endl;  // вызовет исключение any is not initialized
+    }
+    catch (const std::exception& e) {
+        std::cout << "Исключение: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
