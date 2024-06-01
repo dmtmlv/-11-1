56b13f72f8caaec77a7b1415f40d523e09c07d97
dmtmlv <154061230+dmtmlv@users.noreply.github.com>
2024-06-01 21:00:44 +0300

    Update 8.cpp

--- C++Basics/Preparing_C++/2 part/8/8.cpp
+++ C++Basics/Preparing_C++/2 part/8/8.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <ctime>
+
+struct Meeting {
+    std::string date;
+    std::string time;
+};
+
+int main() {
+    std::map<int, Meeting> meetings; // Map для хранения встреч по уникальному ID
+
+    // Добавление новой встречи
+    Meeting newMeeting;
+    newMeeting.date = "2022-12-31";
+    newMeeting.time = "15:00";
+    int meetingID = 1;
+    meetings.insert(std::make_pair(meetingID, newMeeting));
+
+    // Удаление существующей встречи
+    int meetingToDeleteID = 1;
+    auto it = meetings.find(meetingToDeleteID);
+    if (it != meetings.end()) {
+        meetings.erase(it);
+        std::cout << "Встреча с ID " << meetingToDeleteID << " была удалена.\n";
+    } else {
+        std::cout << "Встреча с ID " << meetingToDeleteID << " не найдена.\n";
+    }
+
+    return 0;
+}
 
